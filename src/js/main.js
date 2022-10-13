bf5a1d24ff83bf4b2b57fc22cdea3354c9a38cf4
AbimaelSB <abimael.barros@yahoo.com>
2022-10-13 16:59:07 -0300

    update file

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,8 @@ function mudarAba(aba1, aba2, conteudo1, conteudo2){
   let b = document.querySelector(aba2);
   let c = document.querySelector(conteudo1);
   let d = document.querySelector(conteudo2);
-  a.style.backgroundcolor = "#262626";
-  b.style.backgroundcolor = "#070d12";
+  a.style.background = "#262626";
+  b.style.background = "#070d12";
   c.style.display = "flex";
   d.style.display = "none";
 }
